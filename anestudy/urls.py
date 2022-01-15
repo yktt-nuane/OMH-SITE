cd548dee525ecc0facc3bca5449080d61de457c6
Tatsuro Yokoyama <tyinnagoyauniversity@gmail.com>
2022-01-15 22:42:34 +0900

    編集コマンド追加

--- anestudy/urls.py
+++ anestudy/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('tags/<slug:slug>', views.tags),
     path('<slug:pk>/', views.posted_article),
     path('posted_article/<slug:pk>/', views.posted_article),
+    path('<slug:postarticle_id>/edit/postarticle/', views.edit_post),
 ]
 
