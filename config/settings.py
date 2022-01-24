402655d7b15daa1fdd5860d9e857b39c25dd7ab2
Tatsuro Yokoyama <tyinnagoyauniversity@gmail.com>
2022-01-24 14:16:50 +0900

    deploy

--- config/settings.py
+++ config/settings.py
@@ -183,13 +183,3 @@ MESSAGE_TAGS = {
  }
 # --------- massage tab with bootstrap alert class ---------------------
 
-"""
-# --- Gamil 送信設定 ---
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
-EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
-# --- Gamil 送信設定 ---
-"""
